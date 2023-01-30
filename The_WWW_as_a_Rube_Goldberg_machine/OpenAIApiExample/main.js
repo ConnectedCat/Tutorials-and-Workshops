b0985f5baf32c67ac72ddade3b8b91f68325ced3
Maxim Safioulline <maxim@connectedcatmedia.com>
2023-01-29 18:32:41 -0800

    Added an example of a simple Vite app to access OpenAI API

--- The_WWW_as_a_Rube_Goldberg_machine/OpenAIApiExample/main.js
+++ The_WWW_as_a_Rube_Goldberg_machine/OpenAIApiExample/main.js
@@ -2,18 +2,18 @@ import './style.css'
 import { Configuration, OpenAIApi } from 'openai'
 
 const config = new Configuration({
-  apiKey: "PUT_YOUR_API_KEY_HERE"
+  apiKey: "YOUR_API_KEY_GOES_HERE"
 })
 
-const openai = new OpenAIApi(configuration);
+const openai = new OpenAIApi(config);
 
 const response = await openai.createImage({
   prompt: "A cat wearing armor riding a TRex into battle",
   n: 1, //number of images requested
   size: "512x512",
-});
+})
 
-console.log(response.data.data[0].url)
+console.log(response)
 
 const img = document.createElement('img')
 img.src = response.data.data[0].url

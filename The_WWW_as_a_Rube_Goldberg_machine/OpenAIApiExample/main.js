fb564908d68d275e7a32903576735ebf2777f354
Maxim Safioulline <maxim@connectedcatmedia.com>
2024-01-26 18:25:15 -0800

    Updated the tutorial text and the Vite example to reflect chages in the OpenAI API

--- The_WWW_as_a_Rube_Goldberg_machine/OpenAIApiExample/main.js
+++ The_WWW_as_a_Rube_Goldberg_machine/OpenAIApiExample/main.js
@@ -1,21 +1,21 @@
 import './style.css'
-import { Configuration, OpenAIApi } from 'openai'
+import OpenAI from "openai"
 
-const config = new Configuration({
-  apiKey: "YOUR_API_KEY_GOES_HERE"
+const openai = new OpenAI({ 
+  apiKey: 'YOUR_API_KEY_HERE',
+  dangerouslyAllowBrowser: true //this is because you are storing your API key right above in your code
 })
 
-const openai = new OpenAIApi(config);
-
-const response = await openai.createImage({
+const response = await openai.images.generate({
+  model: "dall-e-3",
   prompt: "A cat wearing armor riding a TRex into battle",
-  n: 1, //number of images requested
-  size: "512x512",
+  n: 1,
+  size: "1024x1024",
 })
 
 console.log(response)
 
 const img = document.createElement('img')
-img.src = response.data.data[0].url
+img.src = response.data[0].url
 img.alt = "A cat wearing armor riding a TRex into battle"
 document.body.appendChild(img)
